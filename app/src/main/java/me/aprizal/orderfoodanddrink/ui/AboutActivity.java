b5daf442b9f02e15648019e65286be22ef33c210
aprizalrizalzal <aprizalrizalzal@gmail.com>
2021-06-09 11:26:23 +0800

    Update Splash And About

--- app/src/main/java/me/aprizal/orderfoodanddrink/ui/AboutActivity.java
+++ app/src/main/java/me/aprizal/orderfoodanddrink/ui/AboutActivity.java
@@ -2,6 +2,7 @@ package me.aprizal.orderfoodanddrink.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,8 +10,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.snackbar.Snackbar;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import me.aprizal.orderfoodanddrink.R;
 
 public class AboutActivity extends AppCompatActivity {
@@ -23,6 +27,10 @@ public class AboutActivity extends AppCompatActivity {
         if (getSupportActionBar()!=null){
             getSupportActionBar().setTitle(R.string.about);
         }
+
+        CircleImageView circleImageView = findViewById(R.id.img_profile);
+        Glide.with(this).load(R.mipmap.ic_profile_round).into(circleImageView);
+
 
         Button btnProfile=findViewById(R.id.btn_profile);
         btnProfile.setOnClickListener(v -> {
--- app/src/main/java/me/aprizal/orderfoodanddrink/ui/SplashActivity.java
+++ app/src/main/java/me/aprizal/orderfoodanddrink/ui/SplashActivity.java
@@ -1,19 +1,28 @@
 package me.aprizal.orderfoodanddrink.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 import me.aprizal.orderfoodanddrink.R;
 
 public class SplashActivity extends Activity {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView imageView = findViewById(R.id.img_app);
+        Glide.with(this).load(R.mipmap.ic_launcher_round).into(imageView);
+
+
         Thread thread = new Thread(){
             @Override
             public void run() {
